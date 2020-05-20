6b60d04971434cea66d8a95f70326a6dbbf0f671
wadamek65 <wadamek65@gmail.com>
2020-05-20 11:26:32 +0200

    Add action for extracting branch name

---
+++ index.ts
@@ -0,0 +1,23 @@
+import { setFailed, debug, error, setOutput, info } from '@actions/core';
+import { context, } from '@actions/github';
+
+try {
+  const allParts = context.ref.split('/');
+  debug(context.ref);
+  info(context.ref);
+  error(context.ref);
+  let branchParts: string[];
+  if (context.ref === 'pull_request') {
+    // Pull request refs are formatted differently like and have an additional part in them
+    // So they need to be handled accordingly
+    branchParts = allParts.slice(3);
+  } else {
+    branchParts = allParts.slice(2);
+  }
+
+  const branch = branchParts.join('_');
+  error(branch);
+  setOutput('branch_name', branch);
+} catch (error) {
+  setFailed(error.message);
+}
